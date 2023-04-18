31df35ba970397280660fbb54a05a548be5bb8cc
Olga Gnezdyonova <ognezdyonova@healthrecoverysolutions.com>
2023-04-18 22:25:59 +0300

    first test

--- src/tests/App.test.js
+++ src/tests/App.test.js
@@ -5,25 +5,27 @@ import App from '../App';
 test('shows 6 products by default', async () => {
     render(<App/>);
 
-    const heading = await screen.findAllByRole('heading', {});
-    expect(heading).to.length(6);
+    const heading = await screen.findAllByRole('heading');
+
+    expect(heading).toHaveLength(6);
 })
 
 test('shows more 6 products after clicking by button', async () => {
     render(<App/>);
 
-    const button = await screen.findAllByRole('button', {
+    const button = await screen.findByRole('button', {
         name: /load more/i
     });
+
     expect(button).toBeVisible();
 
     await user.click(button);
 
-    await waitFor(async () => {
+    await waitFor( async () => {
 
-        const heading = await screen.findAllByRole("heading");
-        expect(heading).to.lessThan(6);
-        expect(heading).to.toHaveLength(12);
+        const heading = await screen.findAllByRole('heading');
+
+        expect(heading).toHaveLength(6);
     })
 })
 
