e0a85cf9b9d93506e23f7cee3c2a104c65aab4a6
Olga Gnezdyonova <ognezdyonova@healthrecoverysolutions.com>
2023-04-19 20:42:07 +0300

    Section 2, Points:18-> const mock = jest.fn();

--- src/__tests__/Section2UserForm.spec.js
+++ src/__tests__/Section2UserForm.spec.js
@@ -82,3 +82,30 @@ test('(b) it calls onUserAdd when the form is submitted', async ()=>{
     expect(listArg[0][0]).toEqual({ name: 'jane', email: 'jane@jane.com' });
 
 })
+
+test('(c-> mock function) it calls onUserAdd when the form is submitted', async ()=>{
+    const mock = jest.fn();
+    //
+    render(<UserForm onUserAdd={mock}/>);
+
+    const nameInput = screen.getByTestId('name');
+    const emailInput = screen.getByTestId('email');
+    const submit = screen.getByTestId('submitButton');
+
+
+    // Simulate typing in a name
+    await user.click(nameInput);
+    await user.keyboard("jane");
+
+    // Simulate typing in an email
+    await user.click(emailInput);
+    await user.keyboard("jane@jane.com");
+
+    //submit form
+    await user.click(submit);
+
+    // assertions
+    expect(mock).toHaveBeenCalled();
+    expect(mock).toHaveBeenCalledWith({name:'jane',email:'jane@jane.com'});
+
+})
